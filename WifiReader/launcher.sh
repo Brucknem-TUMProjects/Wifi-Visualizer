7d7026c16519e44f4ca7b58f5fa6616c0e102a9e
Brucknem <mbruckner94@gmail.com>
2018-12-08 22:41:43 +0100

    Removed Pi stuff

--- WifiReader/launcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /home/pi/Wifi-Visualizer/WifiReader
-sudo python joystick.py
-cd /
