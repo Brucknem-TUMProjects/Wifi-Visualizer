f9ee2d0aecd3b9f1f0bea02e8887287a30af4205
Brucknem <mbruckner94@gmail.com>
2018-12-14 11:27:41 +0100

    Init branch

---
+++ WifiVisualizer/Assets/_Scripts/Handy.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Handy : MonoBehaviour
+{
+    public Text text;
+    public Button SetButton;
+    public Button GetButton;
+
+    private bool toggleWifi = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetButton.onClick.AddListener(delegate { OnSetButton(); });
+        GetButton.onClick.AddListener(delegate { OnGetButton(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSetButton()
+    {
+        try
+        {
+            text.text = "Wifi successfully set: " + SetWifiEnabled(toggleWifi = !toggleWifi);
+        }catch(Exception e)
+        {
+            text.text = "Error in SetWifiEnabled()\n" + e.Message;
+        }
+    }
+
+    public void OnGetButton()
+    {
+        try
+        {
+            text.text = "Wifi is enabled: " + IsWifiEnabled();
+        }
+        catch (Exception e)
+        {
+            text.text = "Error in IsWifiEnabled()\n" + e.Message;
+        }
+    }
+
+    public bool SetWifiEnabled(bool enabled)
+    {
+        bool success = false;
+        using (AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+        {
+            using (var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi"))
+            {
+                success = wifiManager.Call<bool>("setWifiEnabled", enabled);
+            }
+        }
+
+        return success;
+    }
+
+    public bool IsWifiEnabled()
+    {
+        bool success = false;
+        using (AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+        {
+            using (var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi"))
+            {
+                success = wifiManager.Call<bool>("isWifiEnabled");
+            }
+        }
+        return success;
+    }
+}
